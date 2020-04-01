858ac8c781059f31610875b691053da3fe7d900d
mrbluesky0123 <onightraino@gmail.com>
2020-04-01 17:10:45 +0900

    qqq

--- src/main/java/toy/mrbluesky/crawlingbff/service/CrawlingRequestService.java
+++ src/main/java/toy/mrbluesky/crawlingbff/service/CrawlingRequestService.java
@@ -6,34 +6,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import toy.mrbluesky.crawlingbff.vo.CrawlingRequest;
 import toy.mrbluesky.crawlingbff.vo.CrawlingResponse;
 import toy.mrbluesky.crawlingbff.vo.clientvo.ExternalCrawlingResponse;
 import toy.mrbluesky.fwk.base.BaseService;
 
+import java.net.URI;
+
+
 @Service
-@NoArgsConstructor
 @Slf4j
-public class CrawlingRequestService extends BaseService {
+@NoArgsConstructor
+public class CrawlingRequestService {
 
-    private RestTemplate restTemplate;
+  private RestTemplate restTemplate;
 
-    @Autowired
-    public CrawlingRequestService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-    // Call external API
-    public String requestCrawling(CrawlingRequest crawlingRequest) {
-        ExternalCrawlingResponse externalCrawlingResponse = restTemplate.getForObject("http://45.77.18.238:8080/mock-mask", ExternalCrawlingResponse.class);
-        return "SUCCESS";
-    }
+  @Autowired
+  public CrawlingRequestService(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
+  // Call external API
+  public String requestCrawling(CrawlingRequest crawlingRequest) {
+    URI requestUri = UriComponentsBuilder.newInstance()
+            .scheme("http")
+            .host("45.77.18.238:8080")
+            .path("/mock-mask")
+            .queryParam("requestKeyword", crawlingRequest.getRequestKeyword())
+            .queryParam("requestWebsite", crawlingRequest.getRequestWebsite())
+            .queryParam("responseMedia", crawlingRequest.getResponseMedia())
+            .build()
+            .encode()
+            .toUri();
+    ExternalCrawlingResponse externalCrawlingResponse = restTemplate.getForObject(requestUri, ExternalCrawlingResponse.class);
 
-    // Call external API
-    public String requestCrawling() {
-        ExternalCrawlingResponse externalCrawlingResponse = restTemplate.getForObject("http://45.77.18.238:8080/mock-mask", ExternalCrawlingResponse.class);
-//        String obj = restTemplate.getForObject("http://45.77.18.238:8080/mock-mask", String.class);
-        log.info(externalCrawlingResponse.toString());
-        return externalCrawlingResponse.toString();
-    }
+    return "SUCCESS";
+  }
+
+  // Call external API
+  public String requestCrawling() {
+    ExternalCrawlingResponse externalCrawlingResponse = restTemplate.getForObject("http://45.77.18.238:8080/mock-mask", ExternalCrawlingResponse.class);
+//    String obj = restTemplate.getForObject("http://45.77.18.238:8080/mock-mask", String.class);
+    log.info(externalCrawlingResponse.toString());
+    return externalCrawlingResponse.toString();
+  }
 
 }
--- src/main/java/toy/mrbluesky/crawlingbff/vo/clientvo/ExternalCrawlingResponse.java
+++ src/main/java/toy/mrbluesky/crawlingbff/vo/clientvo/ExternalCrawlingResponse.java
@@ -14,6 +14,6 @@ public class ExternalCrawlingResponse {
 
     private int code;
     private String message;
-    private BaseResponseData data;
+    private ExternalCrawlingResponseData data;
 
 }
